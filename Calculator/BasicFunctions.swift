15e0dfa9b35f1e7f1e569df4e7ba650e3000fb34
Manish <manishagarwal428728@gmail.com>
2024-07-04 23:43:13 +0530

    basic functions

---
+++ Calculator/BasicFunctions.swift
@@ -0,0 +1,8 @@
+//
+//  BasicFunctions.swift
+//  Calculator
+//
+//  Created by Manish Agarwal on 04/07/24.
+//
+
+import Foundation
---
+++ Calculator/StandardStyle.swift
@@ -0,0 +1,8 @@
+//
+//  StandardStyle.swift
+//  Calculator
+//
+//  Created by Manish Agarwal on 04/07/24.
+//
+
+import Foundation
