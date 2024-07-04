651f5e896bb00afd721c160854786364f3a45312
Manish <manishagarwal428728@gmail.com>
2024-07-04 19:08:34 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Manish Agarwal on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Manish Agarwal on 04/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
